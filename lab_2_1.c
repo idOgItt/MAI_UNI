f6151703f5734b90cc1ae797cb677ce6480040d6
idOgItt <sergeylkd@bk.ru>
2023-10-17 21:44:38 +0300

    Reworked -c flag

--- lab_2_1.c
+++ lab_2_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 enum len_status_code {len_ok, len_null_string};
 
@@ -14,7 +15,7 @@ enum concationation_string_status_code {concatenate_string_memmory_allocation_er
 
 enum reorder_string_status_code {reorder_string_null_string, reorder_string_memmory_allocation_error, reorder_string_ok, reorder_string_concatination_fail};
 
-enum len_status_code len(char* string, int* length);
+enum len_status_code len(const char* string, int* length);
 
 enum reverse_string_status_code reverse_string(char* string, char** reversed_string);
 
@@ -22,9 +23,9 @@ enum sting_upper_status_code upper_string(char* string, char** uppered_string);
 
 enum strcatty_status_code strcatty(char** concatenated_string, const char* string);
 
-enum reorder_string_status_code reorder_string(char* string, char** reordered_string);
+enum reorder_string_status_code reorder_string(const char* string, char** reordered_string);
 
-enum concationation_string_status_code concatenate_string(char** concatenated_string, int seed, char* string, ...);
+enum concationation_string_status_code concatenate_string(char** concatenated_string, int seed, char** strings, int count);
 
 int main (int argc, char* argv[]){
 
@@ -49,132 +50,141 @@ int main (int argc, char* argv[]){
     char* uppered_string;
     char* reordered_string;
     char* concatenated_string;
-    int seed;
+    unsigned int seed;
     int num_strings;
     char** strings;
 
-    switch (flag)
-    {
-    case 'l':
-        switch(len(string, &length)){
-            case len_ok:
-                printf("The length of the string is: %d\n", length);
-                break;
+    switch (flag){
+        case 'l':
+            switch(len(string, &length)){
+                case len_ok:
+                    printf("The length of the string is: %d\n", length);
+                    break;
 
-            case len_null_string:
-                printf("It is a NULL\n");
-                return 1;
-        }
-        break;
-    
-    case 'r':
-        switch(reverse_string(string, &reversed_string)){
-            case reversed_string_memmory_allocation_error:
-                printf("Memory allocation error\n");
-                return 1;
-
-            case reversed_string_null_string:
-                printf("It is a NULL\n");
-                return 1;
-
-            case reversed_string_ok:
-                printf("The reversed string is: %s\n", reversed_string);
-                free(reversed_string);
-                break;
-        }
-        break;
-
-    case 'u':
-        switch(upper_string(string, &uppered_string)){
-            case uppered_string_memmory_allocation_error:
-                printf("Memory allocation error\n");
-                return 1;
-
-            case upper_string_null_string:
-                printf("It is a NULL\n");
-                return 1;
-
-            case upper_string_ok:
-                printf("The uppered string is: %s\n", uppered_string);
-                free(uppered_string);
-                break;
-        }
-        break;
-    
-    case 'n':
-        switch (reorder_string(string, &reordered_string)){
-            case reorder_string_null_string:
-                printf("It is a NULL\n");
-                return 1;
-        
-            case reorder_string_memmory_allocation_error:
-                printf("Memory allocation error\n");
-                return 1;
-            
-            case reorder_string_concatination_fail:
-                printf("The formation of the string went wrong\n");
-                return 1;
-
-            case reorder_string_ok:
-                printf("The reordered string is: %s\n", reordered_string);
-                free(reordered_string);
-                break;
-        }
-        break;
-
-    case 'c':
-        if (!(argc > 3)){
-            printf("Not enought parameters for this flag: <flag> <string> <seed> <...(strings)>\n");
-            return 1;
-        }
-        seed = atoi(argv[3]);
-        num_strings = argc - 4;
-        strings = (char**)malloc(num_strings * sizeof(char*));
-
-        if (strings == NULL) {
-            printf("Memory allocation error\n");
-            return 1;
-        }
-
-        for(int i = 4; i < argc; i++){
-            strings[i - 4] = argv[i];
-        }
-
-        switch (concatenate_string(&concatenated_string, seed, string, strings))
-        {
-        case concatenate_string_concatination_fail:
-            printf("The conctatinations of strings fail\n");
-            free(strings);
-            return 1;
-        
-        case concatenate_string_memmory_allocation_error:
-            printf("Memory allocation error\n");
-            free(strings);
-            return 1;
-
-        case concatenate_string_null:
-            printf("It is a NULL\n");
-            free(strings);
-            return 1;
-        
-        case concatenation_string_ok:
-            printf("The resulted concatenated string is: %s\n", concatenated_string);
-            free(concatenated_string);
-            free(strings);
+                case len_null_string:
+                    printf("It is a NULL\n");
+                    return 1;
+            }
             break;
-        }
-        break;
+    
+        case 'r':
+            switch(reverse_string(string, &reversed_string)){
+                case reversed_string_memmory_allocation_error:
+                    printf("Memory allocation error\n");
+                    return 1;
 
-    default:
-        printf("There is no such a flag: <-l> <-r> <-u> <-n> <-c>\n");
-        return 1;
-        break;
+                case reversed_string_null_string:
+                    printf("It is a NULL\n");
+                    return 1;
+
+                case reversed_string_ok:
+                    printf("The reversed string is: %s\n", reversed_string);
+                    free(reversed_string);
+                    break;
+            }
+            break;
+
+        case 'u':
+            switch(upper_string(string, &uppered_string)){
+                case uppered_string_memmory_allocation_error:
+                    printf("Memory allocation error\n");
+                    return 1;
+
+                case upper_string_null_string:
+                    printf("It is a NULL\n");
+                    return 1;
+
+                case upper_string_ok:
+                    printf("The uppered string is: %s\n", uppered_string);
+                    free(uppered_string);
+                    break;
+            }
+            break;
+    
+        case 'n':
+            switch (reorder_string(string, &reordered_string)){
+                case reorder_string_null_string:
+                    printf("It is a NULL\n");
+                    return 1;
+        
+                case reorder_string_memmory_allocation_error:
+                    printf("Memory allocation error\n");
+                    return 1;
+            
+                case reorder_string_concatination_fail:
+                    printf("The formation of the string went wrong\n");
+                    return 1;
+
+                case reorder_string_ok:
+                    printf("The reordered string is: %s\n", reordered_string);
+                    free(reordered_string);
+                    break;
+            }
+            break;
+
+        case 'c':
+            if (!(argc > 3)){
+                printf("Not enought parameters for this flag: <flag> <string> <seed> <...(strings)>\n");
+                return 1;
+            }
+            seed = atoi(argv[3]);
+            num_strings = argc - 4;
+        
+            if (num_strings <= 0) {
+                printf("No strings provided to concatenate.\n");
+                return 1;
+            }
+
+            strings = (char**)malloc((num_strings + 1)* sizeof(char*));
+
+            if (strings == NULL) {
+                printf("Memory allocation error\n");
+                return 1;
+            }
+
+            strings[0] = string;
+
+            for(int i = 4; i < argc; i++){
+                strings[i - 3] = argv[i];
+            }
+
+            switch (concatenate_string(&concatenated_string, seed, strings, num_strings)){
+                case concatenate_string_concatination_fail:
+                    printf("The conctatinations of strings fail\n");
+                    free(strings);
+                    free(concatenated_string);
+                    return 1;
+        
+                case concatenate_string_memmory_allocation_error:
+                    printf("Memory allocation error\n");
+                    free(strings);
+                    free(concatenated_string);
+                    return 1;
+
+                case concatenate_string_null:
+                    printf("It is a NULL\n");
+                    free(strings);
+                    free(concatenated_string);
+                    return 1;
+            
+                case concatenation_string_ok:
+                    printf("The resulted concatenated string is: %s\n", concatenated_string);
+                    free(concatenated_string);
+                    free(strings);
+                    break;
+            }
+            break;
+
+        default:
+            printf("There is no such a flag: <-l> <-r> <-u> <-n> <-c>\n");
+            return 1;
+            break;
     }
     
     return 0;
 }
 
-enum len_status_code len(char* string, int* length){
+enum len_status_code len(const char* string, int* length){
 
     if(string == NULL){
         return len_null_string;
@@ -252,25 +262,39 @@ enum strcatty_status_code strcatty(char** concatenated_string, const char* strin
         return strcatty_memmory_allocation_fail;
     }
 
-    while(*string != '\0'){
-        *(*concatenated_string) = *string;
-        string++;
-        (*concatenated_string)++;
+    char* end = *concatenated_string;
+    while (*end != '\0') {
+        end++;
     }
+
+    while (*string != '\0') {
+        *end = *string;
+        end++;
+        string++;
+    }
+
     return strcatty_ok;
 }
 
-enum reorder_string_status_code reorder_string(char* string, char** reordered_string){
+enum reorder_string_status_code reorder_string(const char* string, char** reordered_string){
 
     int length;
+    char* digit_segment, *letter_segment, *other_segment;
 
     if(len(string, &length) == len_null_string){
         return reorder_string_null_string;
     }
 
-    *reordered_string = (char*)malloc(length + 3);
+    *reordered_string = (char*)malloc(length + 1);
+    digit_segment = (char*)malloc(length + 1);
+    letter_segment = (char*)malloc(length + 1);
+    other_segment = (char*)malloc(length + 1);
 
-    if (*reordered_string == NULL){
+    if (*reordered_string == NULL || digit_segment == NULL || letter_segment == NULL || other_segment == NULL){
+        free(*reordered_string);
+        free(digit_segment);
+        free(letter_segment);
+        free(other_segment);
         return reorder_string_memmory_allocation_error;
     }
 
@@ -280,42 +304,55 @@ enum reorder_string_status_code reorder_string(char* string, char** reordered_st
 
     for(int i = 0; i < length; i ++){
         if (isdigit(string[i])){
-            (*reordered_string)[digit_start++] = string[i]; 
+            digit_segment[digit_start++] = string[i]; 
         } else if (isalpha(string[i])) {
-            (*reordered_string)[letter_start++] = string[i];
+            letter_segment[letter_start++] = string[i];
         } else {
-            (*reordered_string)[other_start++] = string[i];
+            other_segment[other_start++] = string[i];
         }
     }
 
-    (*reordered_string)[digit_start] = '\0';
-    (*reordered_string)[letter_start] = '\0';
-    (*reordered_string)[other_start] = '\0';
+    digit_segment[digit_start] = '\0';
+    letter_segment[letter_start] = '\0';
+    other_segment[other_start] = '\0';
 
-    if(strcatty(&reordered_string, *reordered_string + digit_start) == strcatty_memmory_allocation_fail){
+    if(strcatty(reordered_string, digit_segment) == strcatty_memmory_allocation_fail){
+        free(digit_segment);
+        free(letter_segment);
+        free(other_segment);
         return reorder_string_concatination_fail;
     }
 
-    if(strcatty(&reordered_string, *reordered_string + letter_start) == strcatty_memmory_allocation_fail){
+    if(strcatty(reordered_string, letter_segment) == strcatty_memmory_allocation_fail){
+        free(digit_segment);
+        free(letter_segment);
+        free(other_segment);
         return reorder_string_concatination_fail;
     }
+
+    if(strcatty(reordered_string, other_segment) == strcatty_memmory_allocation_fail){
+        free(digit_segment);
+        free(letter_segment);
+        free(other_segment);
+        return reorder_string_concatination_fail;
+    }
+
+    free(digit_segment);
+    free(letter_segment);
+    free(other_segment);
 
     (*reordered_string)[length] = '\0';
 
     return reorder_string_ok;
 }
 
-enum concationation_string_status_code concatenate_string(char** concatenated_string, int seed, char* string, ...){
-    va_list args;
-    va_start(args, string);
+enum concationation_string_status_code concatenate_string(char** concatenated_string, int seed, char** strings, int count){
 
-    int count = 0;
-    const char* arg;
-    while((arg = va_arg(args, const char*)) != NULL){
-        count++;
+    char* string;
+
+    if (count == 0) {
+        return concatenate_string_null;
     }
-
-    va_start(args, string);
 
     srand((unsigned int) seed);
 
@@ -324,23 +361,25 @@ enum concationation_string_status_code concatenate_string(char** concatenated_st
         return concatenate_string_memmory_allocation_error;
     }
 
-    for (int i = 0; i < count; i++){
+    for (int i = 0; i <= count; i++){
         indicies[i] = i;
     }
 
-    for(int i = count - 1; i > 0; i--){
+    for(int i = count; i >= 0; i--){
         int j = rand() % (i + 1);
-        indicies[i] ^= indicies[j];
-        indicies[j] ^= indicies[i];
-        indicies[i] ^= indicies[j];
+        int temp = indicies[i];
+        indicies[i] = indicies[j];
+        indicies[j] = temp;
     }
 
     int total_length = 0;
     int length;
-    for (int i = 0; i < count; i++){
-        if(len(va_arg(args, const char*), &length) == len_ok){
+    for (int i = 0; i <= count; i++){
+        string = strings[indicies[i]];
+        if(len(string, &length) == len_ok){
             total_length += length;
         } else{
+            free(indicies);
             return concatenate_string_null;
         }
     }
@@ -352,22 +391,23 @@ enum concationation_string_status_code concatenate_string(char** concatenated_st
     }
 
     char* dest = *concatenated_string;
-    for (int i = 0; i < count; i++){
-        arg = va_arg(args, const char*);
+    for (int i = 0; i <= count; i++){
+        string = strings[indicies[i]];
         int index = indicies[i];
-        if(strcatty(&dest, arg) == strcatty_ok){
-            if(len(arg, &length) != len_ok) {
+
+        if(strcatty(&dest, string) == strcatty_ok){
+            if(len(string, &length) != len_ok) {
+                free(indicies);
                 return concatenate_string_null;
             }
             dest += length;
         } else{
+            free(indicies);
             return concatenate_string_concatination_fail;
         }
     }
 
     *dest = '\0';
-    **concatenated_string = *dest;
-
 
     free(indicies);
 
