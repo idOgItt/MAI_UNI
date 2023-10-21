c7699b3ed952384fa679a23b7e5a30280ac20712
idOgItt <sergeylkd@bk.ru>
2023-10-21 14:17:53 +0300

    Commited lab

---
+++ lab_2_9.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+
+enum define_number_float_status_code {
+    define_number_float_ok,
+    define_number_float_fail
+};
+
+enum define_number_float_status_code define_number_float(double*** result, int base, int count, ...) {
+    va_list numbers;
+    va_start(numbers, count);
+
+    *result = (double**)malloc(count * sizeof(double*));
+    for (int i = 0; i < count; i++) {
+        (*result)[i] = (double*)malloc(2 * sizeof(double));
+    }
+
+    if (*result == NULL) {
+        return define_number_float_fail;
+    }
+
+    for (int i = 0; i < count; i++) {
+        double number = va_arg(numbers, double);
+        if (number <= 0 || number >= 1) {
+            for (int j = 0; j < count; j++) {
+                free((*result)[j]);
+            }
+            free(*result);
+            va_end(numbers);
+            return define_number_float_fail;
+        }
+
+        (*result)[i][0] = number;
+        double denominator = 1e8;
+        int numerator = (int)(number * denominator);
+        int isFinite = 1;
+
+        int gcd = 1;
+        int a = numerator;
+        int b = denominator;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        gcd = a;
+
+        numerator /= gcd;
+        denominator /= gcd;
+
+        // Проверяем, имеет ли дробь конечное представление в системе с заданным основанием.
+        if (fmod(denominator, base) != 0) {
+            isFinite = 0;
+        }
+
+        (*result)[i][1] = isFinite;
+    }
+
+    va_end(numbers);
+    return define_number_float_ok;
+}
+
+int main() {
+    int base = 7;
+    double real_1 = 0.0101001;
+    double real_2 = 0.0064;
+    double real_3 = 0.026;
+    double** result;
+    int count = 3;
+
+    if (define_number_float(&result, base, count, real_1, real_2, real_3) == define_number_float_ok) {
+        for (int i = 0; i < count; i++) {
+            printf("The define of the number %f in the system base %d -> %sfinite\n", result[i][0], base, result[i][1] ? "" : "in");
+        }
+
+        for (int i = 0; i < count; i++) {
+            free(result[i]);
+        }
+        free(result);
+    } else {
+        printf("The mistake occurred\n");
+        return 1;
+    }
+    return 0;
+}
