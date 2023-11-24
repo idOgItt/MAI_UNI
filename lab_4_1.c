4da8a8857fdd04f23e155e686c573cb023b663eb
stardust <sergeylkd@bk.ru>
2023-11-24 22:41:17 +0300

    To point out status_code

--- lab_4_1.c
+++ lab_4_1.c
@@ -33,6 +33,30 @@ bool is_prime(int num) {
     return true;
 }
 
+void trim(char *str) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    // Trim leading spaces and newline characters
+    while (start < length && (str[start] == ' ' || str[start] == '\n')) {
+        start++;
+    }
+
+    // Trim trailing spaces and newline characters
+    while (end > start && (str[end] == ' ' || str[end] == '\n')) {
+        end--;
+    }
+
+    // Shift characters to the beginning
+    for (int i = start; i <= end; i++) {
+        str[i - start] = str[i];
+    }
+
+    // Null-terminate the trimmed string
+    str[end - start + 1] = '\0';
+}
+
 // Prime_number > given_number
 int next_prime_after(int n) {
     int next_number = n + 1;
@@ -57,16 +81,18 @@ unsigned int calculate_key(const char* string) {
 
     while (*string) {
         int digit = 0;
-
+        
         if (*string >= 'a' && *string <= 'z') {
             digit = *string - 'a' + 36;
+            key = key * base + digit;
         } else if (*string >= 'A' && *string <= 'Z') {
             digit = *string - 'A' + 10;
+            key = key * base + digit;
         } else if (*string >= '0' && *string <= '9') {
             digit = *string - '0';
+            key = key * base + digit;
         }
 
-        key = key * base + digit;
         string++;
     }
 
@@ -93,7 +119,7 @@ const char* get_value_from_cache(Hash_Cache* cache, const char* string_key, int 
         node = node->next;
     }
 
-    // The entry is not in the cache, calculate the hash and add a new node to the chain
+    // The entry is not in the cache
     return NULL;
 }
 
@@ -108,50 +134,60 @@ void free_hash_cache(Hash_Cache* cache) {
             free(current);
             current = next;
         }
+        // Set the next pointer to NULL after freeing the chain
+        cache->entries[i].next = NULL;
     }
     free(cache->entries);
 }
 
-// Function to rehash the table
-void rehash_cache(Hash_Cache* old_cache, int new_size) {
-    Hash_Cache new_cache;
-    create_cache(&new_cache, new_size);
 
-    for (int i = 0; i < old_cache->size; i++) {
-        Hash_Cash_Node* node = &old_cache->entries[i];
+
+// Function to rehash the table
+void rehash_cache(Hash_Cache** old_cache, Hash_Cache** new_cache, int new_size) {
+    // Create a new cache
+    Hash_Cache* temp_new_cache = (Hash_Cache*)malloc(sizeof(Hash_Cache));
+    create_cache(temp_new_cache, new_size);
+
+    for (int i = 0; i < (*old_cache)->size; i++) {
+        Hash_Cash_Node* node = &((*old_cache)->entries[i]);
         while (node->next != NULL) {
             unsigned int key = node->next->key;
-            unsigned int hash = node->next->hash;
+            char* value = node->next->value;
             unsigned int new_hash = key % new_size;
 
-            Hash_Cash_Node* new_node = &new_cache.entries[new_hash];
+            Hash_Cash_Node* new_node = &((temp_new_cache)->entries[new_hash]);
             while (new_node->next != NULL) {
                 new_node = new_node->next;
             }
 
             new_node->next = (Hash_Cash_Node*)malloc(sizeof(Hash_Cash_Node));
-            /*if (new_node->next == NULL) {
-                perror("Error allocating memory");
-                return -1;
-            }*/
             new_node->next->key = key;
-            new_node->next->hash = hash;
+            new_node->next->hash = new_hash;
+            new_node->next->value = value;
             new_node->next->next = NULL;
 
-            // Move to next node and delete old one
-            Hash_Cash_Node* temp = node->next;
             node->next = node->next->next;
-            free(temp);
         }
     }
 
-    free_hash_cache(old_cache);
+    // Free the old cache
+    free_hash_cache(*old_cache);
 
-    memcpy(old_cache, &new_cache, sizeof(Hash_Cache));
+    // Update the size of the old cache
+    (*old_cache)->size = new_size;
+
+    // Swap old_cache and temp_new_cache
+    Hash_Cache* temp = *old_cache;
+    *old_cache = temp_new_cache;
+    temp_new_cache = temp;
+    
+    // Update the new_cache pointer in the calling code
+    *new_cache = temp_new_cache;
 }
 
+
 // Function to check the load_factor of chains, to resize or not
-void check_and_resize(Hash_Cache* cache) {
+void check_and_resize(Hash_Cache* cache, Hash_Cache* new_cache) {
     int LOAD_FACTOR = 2;
     int shortest_chain = 2;
     int longest_chain = 0;
@@ -164,11 +200,7 @@ void check_and_resize(Hash_Cache* cache) {
             chain_length++;
             node = node->next;
         }
-
-        // Update shortest and longest chains
-        if (chain_length < shortest_chain) {
-            shortest_chain = chain_length;
-        }
+        
         if (chain_length > longest_chain) {
             longest_chain = chain_length;
         }
@@ -177,12 +209,14 @@ void check_and_resize(Hash_Cache* cache) {
     // Check for significant difference
     if (longest_chain >= LOAD_FACTOR * shortest_chain) {
         int new_size = next_prime_after(cache->size);
-        rehash_cache(cache, new_size);
+        rehash_cache(&cache, &new_cache, new_size);
     }
 }
 
 // Function to insert a key and hash into the hash table
-void insert_into_cache(Hash_Cache* cache, unsigned int key, unsigned int hash, const char* value) {
+void insert_into_cache(Hash_Cache* cache, unsigned int key, unsigned int hash, const char* value, Hash_Cache* new_cache) {
+
+    // Element does not exist, proceed with insertion
     Hash_Cash_Node* new_node = (Hash_Cash_Node*)malloc(sizeof(Hash_Cash_Node));
     new_node->key = key;
     new_node->hash = hash;
@@ -191,7 +225,24 @@ void insert_into_cache(Hash_Cache* cache, unsigned int key, unsigned int hash, c
     new_node->value[strlen(value)] = '\0'; // NULL Terminate
     new_node->next = cache->entries[hash].next;
     cache->entries[hash].next = new_node;
+
+    // Call check_and_resize after inserting an element
+    check_and_resize(cache, new_cache);
 }
+
+void insert_into_cache_file(Hash_Cache* cache, const char* string_key, const char* value, Hash_Cache* new_cache){
+
+    const char* existing_value = get_value_from_cache(cache, string_key, cache->size);
+    if (existing_value != NULL) {
+        // Element already exists, no need to insert
+        return;
+    }
+
+    unsigned int key = calculate_key(string_key);
+    unsigned int hash = calculate_hash(string_key, cache->size);
+    insert_into_cache(cache, key, hash, value, new_cache);
+}
+
 
 // Function to handle input file
 enum handle_input_file_status_code handle_input_file(char* input_file_path) {
@@ -210,7 +261,7 @@ enum handle_input_file_status_code handle_input_file(char* input_file_path) {
 
     int HASHSIZE = 128;
 
-    Hash_Cache cache;
+    Hash_Cache cache, new_cache;
     create_cache(&cache, HASHSIZE);
 
     char line[256];
@@ -222,18 +273,32 @@ enum handle_input_file_status_code handle_input_file(char* input_file_path) {
             if (sscanf(line, "#define %s %s\n", def_name, value) == 2) {
                 // Calculate hash and insert into the cache
                 printf("%s %s\n", def_name, value);
-                unsigned int key = calculate_key(def_name);
-                unsigned int hash = calculate_hash(def_name, HASHSIZE);
-                insert_into_cache(&cache, key, hash, value);
+                insert_into_cache_file(&cache, def_name, value, &new_cache);
             }
         } else {
             // Replace occurrences of <def_name> with <value>
-            char* token = strtok(line, " ");
+            char* token = strtok(line, " \n");
             while (token != NULL) {
+                int length = strlen(token);
+                printf("%s\n", token);
+                if (token[strlen(token) - 1] == '\n' && token[strlen(token) - 2] == ';'){
+                    token[strlen(token) - 2] = '\n';
+                    token[strlen(token) - 1] = '\0';
+                }
+                if (token[strlen(token) - 1] == ';'){
+                    printf("%s\n", token);
+                    token[strlen(token) -1] = '\0';
+                    printf("%s\n", token);
+                }
                 const char* value = get_value_from_cache(&cache, token, HASHSIZE);
                 if (value != NULL) {
                     printf("%s\n",value);
-                    fprintf(output_file, "%s ", value);
+                    if (token[length - 2] == '\n'){
+                        token[length - 2] = '\0';
+                        fprintf(output_file, "%s\n", value);
+                    } else {
+                        fprintf(output_file, "%s ", value);
+                    }
                 } else {
                     fprintf(output_file, "%s ", token);
                 }
