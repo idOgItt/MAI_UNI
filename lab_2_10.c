c408f94b85b7c4a5f73fdd93b8f824280814073b
stardust <sergeylkd@bk.ru>
2023-12-04 21:49:49 +0300

    Done

--- lab_2_10.c
+++ lab_2_10.c
@@ -21,44 +21,42 @@ enum polynomial_status_code polynomial_expansion(
         return polynomial_fail;
     }
 
-    for (int i = 0; i <= degree; i++) {
-        (*result)[i] = 0.0;
-    }
-
-    double x = 1;
-    double current_coefficient;
-
-    if ( a == 0){
-        for(int i = 0; i <= degree; i++){
+    // Если a равно 0, просто копируем коэффициенты
+    if (a == 0) {
+        for (int i = 0; i <= degree; i++) {
             (*result)[i] = va_arg(coefficients, double);
         }
         va_end(coefficients);
         return polynomial_ok;
     }
 
-    for (int i = 0; i <= degree; i++) {
-        current_coefficient = va_arg(coefficients, double);
-        double binomaial = 1.0;
-        double factorial = 1.0;
-
-        for (int j = 0; j <= i; j++){
-            factorial *=(j + 1);
-        }
-        
-        binomaial = i / factorial;
-        (*result)[i] += current_coefficient * binomaial;
-        /*for (int j = i; j <= degree; j++) {
-            double binomial = 1.0;
-
-            for (int k = 0; k < i; k++) {
-                binomial *= (x) / (k + 1);
-            }
-
-            (*result)[j] += current_coefficient * binomial * (i);
-        } */
+    double *temp = (double *)malloc((degree + 1) * sizeof(double));
+    if (temp == NULL) {
+        free(*result);
+        va_end(coefficients);
+        return polynomial_fail;
     }
 
+    for (int i = 0; i <= degree; i++) {
+        temp[i] = va_arg(coefficients, double);
+    }
     va_end(coefficients);
+
+    // Расчет коэффициентов результирующего многочлена
+    for (int i = 0; i <= degree; i++) {
+        double term = 1.0;
+        double sum = temp[i];
+
+        for (int j = 1; j <= i; j++) {
+            term *= (a - j + 1) / j;
+            sum += term * temp[i - j];
+        }
+
+        (*result)[i] = sum;
+    }
+
+    free(temp);
+
     return polynomial_ok;
 }
 
@@ -68,14 +66,19 @@ int main() {
     double *result;
     int degree = 3;
     double coeff0 = 1.0;
-    double coeff1 = 3.0;
-    double coeff2 = 2.0;
-    double coeff3 = 4.0;
+    double coeff1 = -4.0;
+    double coeff2 = 0.0;
+    double coeff3 = 1.0;
 
     if (polynomial_expansion(epsilon, a, &result, degree, coeff0, coeff1, coeff2, coeff3) == polynomial_ok) {
-        for (int i = 0; i <= degree; i++) {
-            printf("Coefficient %d: %f\n", i, result[i]);
+        printf("Original polynomial: f(x) = %f + %fx + %fx^2 + %fx^3\n", coeff0, coeff1, coeff2, coeff3);
+
+        printf("Expanded polynomial: f(x) = %f", result[0]);
+        for (int i = 1; i <= degree; i++) {
+            printf(" + %f(x - %.2lf)^%d", result[i], a, i);
         }
+        printf("\n");
+
         free(result);
     } else {
         printf("The expansion failed\n");
